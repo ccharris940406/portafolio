8f5d36966083cf18e3ea395a454b6665eac7822f
Carlos César Harris Castillo <47110071+ccharris940406@users.noreply.github.com>
2025-05-26 19:34:43 -0600

    Update layout.tsx
    Seo optimisation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,28 @@ import NavBar from "./components/nav-bar";
 import "./globals.css";
 
 export const metadata = {
-  title: "Harri's Portafolio ",
+  title: "Harri's Portafolio",
   description: "Personal Portafolio",
+  openGraph: {
+    title: "Harri's Portafolio",
+    description: "Personal Portafolio",
+    url: "https://ccharris.vercel.app",
+    siteName: "Harri's Portafolio",
+    images: [
+      {
+        url: "", // URL de la imagen para compartir
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Harri's Portafolio",
+    description: "Personal Portafolio",
+    images: [""],
+  },
 };
 
 export default function RootLayout({
